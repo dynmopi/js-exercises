00a79e2e5e0ae4ff1dee013173838894bdf20ee9
Danrley Maranhão <maranhaodanrley@gmail.com>
2024-06-13 20:22:19 -0300

    js-exercises

---
+++ exercises/003-statements/001_node.js
@@ -0,0 +1,27 @@
+//dica: em todas as declarações, utilize o ponto e vírgula (recomendado).
+//apesar que o js não obriga a utilização.
+
+let x, y, z; // indicando as variaveis
+x = 5; // declaração 1
+y = 4; // declaração 2
+z = 5 + 4; // declaração 3
+
+console.log(z)
+
+//declaração em java script, é o que diz esse sinal de igual    
+
+//existem as declarações em blocos, exemplo disso são as functions
+
+escrever() // para chamar a função
+
+function escrever(){
+    console.log("escrevendo");
+}
+
+/*cuidado! palavras-chaves não podem ser utilizadas para nomes de variáveis
+    let
+    if
+    var
+    switch
+    const
+*/

ba26d239f11d63f319392132f342370ad02b5699
Danrley Maranhão <maranhaodanrley@gmail.com>
2024-06-17 20:34:58 -0300

    js-exercises

--- exercises/006_operators/002_aritmethic_assignment.js
+++ exercises/006_operators/002_aritmethic_assignment.js
@@ -1,4 +1,36 @@
 /*
 operadores aritmeticos: sao as operacoes classicas da matematica...
-operadores de declaracao: 
+operadores de declaracao: aquelas que declaram as variaveis
+*/
+
+//aritmeticos
+let a = 4;
+let x = (100 + 50) * a;
+console.log(x); // retorna 600
+
+/*
+    +	Addition
+    -	Subtraction
+    *	Multiplication
+    **	Exponentiation (ES2016)
+    /	Division
+    %	Modulus (Division Remainder) ou (Resto da divisao)
+    ++	Increment (x++ -> x = x + 1)
+    --	Decrement (x-- -> x = x - 1)
+*/
+
+//declarativos, uma forma de simplificar o codigo
+let n = 10;
+n += 10;
+
+/*
+
+=	x = y	x = y
++=	x += y	x = x + y
+-=	x -= y	x = x - y
+*=	x *= y	x = x * y
+/=	x /= y	x = x / y
+%=	x %= y	x = x % y
+**=	x **= y	x = x ** y
+
 */

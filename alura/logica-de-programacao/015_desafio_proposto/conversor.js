b14b6924510b66408eda5cafc0b78b65ea73a674
Danrley Maranhão <maranhaodanrley@gmail.com>
2024-06-21 15:42:40 -0300

    alura-logica-de-programacao

--- alura/logica-de-programacao/015_desafio_proposto/conversor.js
+++ alura/logica-de-programacao/015_desafio_proposto/conversor.js
@@ -1,8 +1,10 @@
-function converter(){
+function converter(real){
     let dolar = 4.80;
+    let converter = real / 4.80;
 
+    return converter;
 }
 
 
 
-console.log(converter(10));
+console.log(converter(48));

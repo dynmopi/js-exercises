120c0ee5252ef5724532d6759836e8ded96316f8
Danrley Maranhão <maranhaodanrley@gmail.com>
2024-06-21 12:30:20 -0300

    alura-ch

--- alura/logica-de-programacao/014_js-curso-2-aula_-/app.js
+++ alura/logica-de-programacao/014_js-curso-2-aula_-/app.js
@@ -11,26 +11,33 @@ paragrafo.innerHTML = 'Digite um número entre 1 a 10'; // innerHTML escreve "de
 let numeroSecreto = numero_secreto(); // chama a funcao para armazenar na variavel. É preciso do return
 let tentativas = 1;
 
+//para evitar código repetido :v
 function exibirTexto(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto; 
 }
 // essas duas funções abaixo, chama afunção exibirTexto() com os respectivos parâmetros.
-exibirTexto('h1', 'Jogo do número secreto'); // tag, texto
-exibirTexto('p', 'Digite um número entre 1 a 10'); // tag, texto
-// lembre-se de funções compostas na matemática
+// lembre-se de funções compostas na matemática ;v
+
+mensagemNaTela();
 
 function verificarChute(){ // é ativado através do onclick
+
     // não se esqueça de colocar o ".value" para especificar que queres pegar somente o valor dentro do input do html
     let chute = document.querySelector('input').value;
+    
     if (chute == numeroSecreto){
         exibirTexto('h1', 'Correto!');
         
-        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
-        let mensagemTentativas = `Você acertou o número secreto com ${tentativas} ${palavraTentativa}`
+        //concordância do português, caso for mais de uma tentativa, será "tentativas"
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você acertou o número secreto com ${tentativas} ${palavraTentativa}`;
 
         exibirTexto('p', mensagemTentativas);
-        document.getElementById('reiniciar').removeAttribute('disabled');
+
+        //se utilizasse o querySelector, ele selecionaria os dois buttons.
+        document.getElementById('reiniciar').removeAttribute('disabled'); 
+
     } else {
         if (chute > numeroSecreto){
             exibirTexto('p', 'O número secreto é menor');
@@ -39,14 +46,33 @@ function verificarChute(){ // é ativado através do onclick
         }
         
         tentativas++;
+        limpar();
     }
 }
 
 function numero_secreto(){
-    return parseInt(Math.random() * 10 + 1); // retorna até aonde foi chamado
+    // retorna até aonde foi chamado
+    return parseInt(Math.random() * 10 + 1); 
 }
 
+// serve para limpar o input
+function limpar(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function mensagemNaTela(){
+    exibirTexto('h1', 'Jogo do número secreto'); 
+    exibirTexto('p', 'Digite um número entre 1 a 10');
+}
+
+
 function reiniciarJogo(){
-    
+    numeroSecreto = numero_secreto();
+    limpar();
+    tentativas = 1;
+
+    mensagemNaTela();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
 // Nome de função não pode ser nome de variável!!
